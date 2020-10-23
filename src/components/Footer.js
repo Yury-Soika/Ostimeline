f8a9c5e19fffa272abb61ca2e9fc09d8650503d2
Yury Soiko <puma10009@gmail.com>
2020-10-23 16:03:43 +0300

    feat: Added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Button } from './Button';
+import './Footer.css';
+import { Link } from 'react-router-dom';
+
+const Footer = () => {
+  return(
+    <div className="footer-container">
+      <section className="social-media">
+        <div className="social-media-wrap">
+          <div className="footer-logo">
+            <Link to="/" className="social-logo">
+              ostimeline
+            </Link>
+          </div>
+          <small className="website-rights">© 2020 Yury Soiko</small>
+          <div className="social-icons">
+            <Link
+              className="social-icon-link vk"
+              to={"//vk.com/id56289661"}
+              target="_blank"
+              aria-label='Vk'
+            >
+              <i className='fab fa-vk' />
+            </Link>
+            <Link
+              className="social-icon-link instagram"
+              to={"//www.instagram.com"}
+              target="_blank"
+              aria-label='Instagram'
+            >
+              <i className='fab fa-instagram' />
+            </Link>
+            <Link
+              className="social-icon-link telegram"
+              to={"//t.me/plexers"}
+              target="_blank"
+              aria-label='Telegram'
+            >
+              <i className='fab fa-telegram' />
+            </Link>
+            <Link
+              className="social-icon-link email"
+              to={"//mailto:puma10009@gmail.com"}
+              target="_blank"
+              aria-label='Email'
+            >
+              <i className='fas fa-envelope' />
+            </Link>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Footer;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import '../../App.css';
 import MainSection from '../MainSection';
 import Cards from '../Cards';
+import Footer from '../Footer';
 
 const Home = () => {
 	return(
     <>
       <MainSection />
       <Cards />
+      <Footer />
     </>
 	);
 }
