7ebb1b06cdba94b3a263ed3adcbfb45a866aa98f
Yury Soiko <puma10009@gmail.com>
2020-11-02 22:34:25 +0300

    feat: added content to project pages

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,8 +1,45 @@
 import React from 'react';
+import Partners from './../Partners';
 import '../../App.css';
 
 const About = () => {
-  return <h1 className="about">ABOUT</h1>;
+  return (
+    <div className="content">
+      <section>
+        <h3 className="title">About ostimeline</h3>
+
+        <p>
+          The ostimeline project makes interactive HTML-based timelines with virtualized operating systems
+          (or any other software) embedded into HTML instead of screenshots.
+        </p>
+
+        <p>
+          It creates a visual timeline to navigate among several pages, each page devoted to some
+          version of an operating system or an application. The primary goal of the project was presenting
+          a live history of GUI-based mobile & desktop operating systems, which looks as follows:
+        </p>
+
+        
+        <img alt="Xerox Alto" src="../../../public/images/xerox_alto.png" width="50%" />
+
+        <p>
+          Pages are showing some description and frames with virtualized operating systems, embedded
+          into text as if they where ordinary screenshots.
+        </p>
+
+        
+        <img alt="timeline with navigation" src="../../../public/images/timeline_nav" width="30%" />
+
+        <p>
+          User can flip through timeline and interact with virtualized systems by mouse.
+          Keyboard interaction with virtual machine is also possible (you just need to click on the border,
+          which takes keyboard focus from the timeline to the virtual machine).
+        </p>
+      </section>
+      
+      <Partners/>
+    </div>
+  )
 }
 
 export default About;
--- src/components/pages/Downloads.js
+++ src/components/pages/Downloads.js
@@ -1,6 +1,6 @@
 import React from 'react';
+import Partners from './../Partners';
 import './../Button.css';
-import './downloads.css';
 import '../../App.css';
 
 const Downloads = () => {
@@ -12,31 +12,29 @@ const Downloads = () => {
 
   return (
     <div className="content downloads">
-      <main>
+      <section>
         <h1 className="download">Downloads</h1>
         <button 
-          className='btn btn--outline btn--large'
+          className='btn btn--primary btn--large'
           onClick={() => download('demo')}
         >
           <i className="fas fa-file-download"></i> Demo
         </button>
         <button 
-          className='btn btn--outline btn--large'
+          className='btn btn--primary btn--large'
           onClick={() => download('ostimeline')}
         >
           <i className="fas fa-file-download"></i> Ostimeline
         </button>
         <button 
-          className='btn btn--outline btn--large'
+          className='btn btn--primary btn--large'
           onClick={() => download('lovelygraybuttons')}
         >
           <i className="fas fa-file-download"></i> Lovely Gray Buttons
         </button>
-      </main>
-      <div className="sidebar">
-        <h3>Partners</h3>
-        <a href="https://www.bstu.by/" className="partners-img"></a>
-      </div>
+      </section>
+      
+      <Partners/>
     </div>
   );
 }
