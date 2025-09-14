71bd6b174de236a75c5acf6385fd1ace8234d08c
Yury Soika <puma10009@gmail.com>
2025-09-14 15:31:09 +0200

    Update Footer component

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,45 +3,45 @@ import './Footer.scss';
 import './Social.scss';
 
 const Footer = () => {
-  return(
-    <div className="footer">
-      <section className="social-media">
-        <div className="social-media-wrap">
-          <div className="footer-logo">
-            <a href="/" className="social-logo">
+  return (
+    <div className='footer'>
+      <section className='social-media'>
+        <div className='social-media-wrap'>
+          <div className='footer-logo'>
+            <a href='/' className='social-logo'>
               ostimeline
             </a>
           </div>
-          <small className="website-rights">© 2021 Yury Soiko</small>
-          <div className="social-icons">
+          <small className='website-rights'>© 2021 Yury Soika</small>
+          <div className='social-icons'>
             <a
-              className="social-icon-link vk"
-              href={"//vk.com/id56289661"}
-              target="_blank"
+              className='social-icon-link vk'
+              href={'//vk.com/id56289661'}
+              target='_blank'
               aria-label='Vk'
             >
               <i className='fab fa-vk' />
             </a>
             <a
-              className="social-icon-link instagram"
-              href={"//www.instagram.com/yura_soiko/"}
-              target="_blank"
+              className='social-icon-link instagram'
+              href={'//www.instagram.com/plexrs/'}
+              target='_blank'
               aria-label='Instagram'
             >
               <i className='fab fa-instagram' />
             </a>
             <a
-              className="social-icon-link telegram"
-              href={"//t.me/plexers"}
-              target="_blank"
+              className='social-icon-link telegram'
+              href={'//t.me/plexers'}
+              target='_blank'
               aria-label='Telegram'
             >
               <i className='fab fa-telegram' />
             </a>
             <a
-              className="social-icon-link email"
-              href={"//mailto:puma10009@gmail.com"}
-              target="_blank"
+              className='social-icon-link email'
+              href={'//mailto:puma10009@gmail.com'}
+              target='_blank'
               aria-label='Email'
             >
               <i className='fas fa-envelope' />
@@ -51,6 +51,6 @@ const Footer = () => {
       </section>
     </div>
   );
-}
+};
 
 export default Footer;
